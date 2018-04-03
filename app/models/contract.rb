72183b1595023c16c35ef40b677d939b01b96df5
Willian <w.goncalves91@gmail.com>
2018-04-02 21:48:39 -0300

    Implement validation to avoid conflicting contracts.

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -3,13 +3,19 @@ class Contract < ApplicationRecord
   belongs_to :user
   validates_presence_of :guide, :user, :start_date, :end_date
   validates :goals, presence: true, length: { minimum: 10 }
-  validate :start_date_must_be_before_end_date
+  validate :start_date_must_be_before_end_date, :contracts_can_not_conflict
 
   def start_date_must_be_before_end_date
     if start_date.present? and end_date.present?
       if start_date > end_date
         errors.add(:start_date, :invalid_interval)
       end
+    end
+  end
+
+  def contracts_can_not_conflict
+    if Contract.where("guide_id = ? AND end_date > ?", guide_id, start_date).any?
+      errors.add(:start_date, :date_conflict)
     end
   end
 end
--- spec/models/contract_spec.rb
+++ spec/models/contract_spec.rb
@@ -16,4 +16,18 @@ RSpec.describe Contract, type: :model do
     it { expect(contract.valid?).to eq false }
     it { expect(contract.errors[:start_date]).to match_array [ I18n.t(:invalid_interval, scope: [:activerecord, :errors, :models, :contract, :attributes, :start_date]) ] }
   end
+
+  describe 'conflict with other contracts of a guide' do
+    before(:all) do
+      @contractor = Fabricate :user
+      user = Fabricate :user
+      @guide = Fabricate :guide, user: user
+      Fabricate :contract, start_date: 1.day.from_now, end_date: 3.days.from_now, guide: @guide, user: @contractor
+    end
+
+    let!(:conflicting_contract) { Fabricate.build :contract, start_date: 2.days.from_now, end_date: 4.days.from_now, guide: @guide, user: @contractor }
+    before { conflicting_contract.validate }
+    it { expect(conflicting_contract.valid?).to eq false }
+    it { expect(conflicting_contract.errors[:start_date]).to match_array [ I18n.t(:date_conflict, scope: [:activerecord, :errors, :models, :contract, :attributes, :start_date]) ] }
+  end
 end
